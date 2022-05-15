b6d8fec539ec996181d3302f5d196ba977d98a71
ZackTheAwkward <zack.e.gaddy@gmail.com>
2022-05-15 11:48:32 -0600

    fixed search by ABV range function, added more elements to make index page more readable

--- BeerTrackerREST/src/main/webapp/js/eventFunctions.js
+++ BeerTrackerREST/src/main/webapp/js/eventFunctions.js
@@ -11,53 +11,53 @@ function init() {
 		if (!isNaN(beerId) && beerId > 0) {
 			getBeer(beerId);
 		}
-		
-		
-		
+
+
+
 	});
-	
-	
+
+
 	//Event Listener for Post Method(Adding Beer)
 	document.createBeerForm.createBeer.addEventListener('click', createBeer);
 
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	//Event Listener for Put Method(Updating Beer)
-	document.updateBeerForm.updateBeer.addEventListener('click', function(event){
+	document.updateBeerForm.updateBeer.addEventListener('click', function(event) {
 		event.preventDefault();
 		let updateId = document.updateBeerForm.updateId.value;
-	console.log(updateId);
+		console.log(updateId);
 		if (!isNaN(updateId) && updateId > 0) {
 			updateBeer(updateId);
-		}		
+		}
 	});
 
 
-document.deleteBeer.lookup.addEventListener('click', function(event){
+	document.deleteBeer.lookup.addEventListener('click', function(event) {
 		event.preventDefault();
 		let deleteId = document.deleteBeer.deleteId.value;
-	console.log(deleteId);
+		console.log(deleteId);
 		if (!isNaN(deleteId) && deleteId > 0) {
 			deleteBeer(deleteId);
-		}		
+		}
 	});
 
-document.findBeerRange.range.addEventListener('click', function(event){
-	event.preventDefault();
+	document.findBeerRange.range.addEventListener('click', function(event) {
+		event.preventDefault();
 		let min = document.findBeerRange.min.value;
 		let max = document.findBeerRange.max.value;
-	console.log(min);
-	console.log(max);
+		console.log(min);
+		console.log(max);
 		if (!isNaN(min) && min > 0) {
 			findBeerByABVRange(min, max);
-		}		
-	
-	
-});
+		}
+
+
+	});
 
 
 
@@ -172,7 +172,7 @@ function updateBeer(updateId) {
 		}
 	};
 	let beer = {
-		id : document.updateBeerForm.updateId.value,
+		id: document.updateBeerForm.updateId.value,
 		name: document.updateBeerForm.name.value,
 		type: document.updateBeerForm.type.value,
 		abv: document.updateBeerForm.abv.value,
@@ -186,36 +186,50 @@ function updateBeer(updateId) {
 
 }
 
-function deleteBeer(deleteId){
-let xhr = new XMLHttpRequest();
+function deleteBeer(deleteId) {
+	let beerDiv = document.getElementById('beerData');
+	beerDiv.textContent = '';
+	let xhr = new XMLHttpRequest();
 	xhr.open('DELETE', 'api/beers/' + deleteId);
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState === 4) {
 			if (xhr.status === 204) {
 				console.log('Deleted');
+				let h2 = document.createElement('h2');
+				h2.textContent = "You have deleted the beer with the ID of " + deleteId;
+				beerDiv.appendChild(h2);
 			}
 		} else {
 			console.log('Beer not found');
 		}
-	}	
+	}
 	xhr.send();
+
 };
 
-function findBeerByABVRange(min, max){
-let xhr = new XMLHttpRequest();
-	xhr.open('GET', 'api/beers/search/abv/'+min+'/'+max);
+function findBeerByABVRange(min, max) {
+	let beerDiv = document.getElementById('beerData');
+	beerDiv.textContent = '';
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET', 'api/beers/search/abv/' + min + '/' + max);
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState === 4) {
 			if (xhr.status === 200) {
 				let beer = JSON.parse(xhr.responseText);
-				for(let i = 0; i < beer.length; i++){
-					displayBeer(beer);
+				let h1 = document.createElement('h1');
+				h1.textContent = 'Beer by ABV% in the range of ' + min + '% and ' + max + '%';
+				beerDiv.appendChild(h1);
+				for (let i = 0; i < beer.length; i++) {
+					let li = document.createElement('li');
+					li.textContent = 'Name: ' + beer[i].name + ' ABV:' + beer[i].abv + '%';
+					beerDiv.appendChild(li);
+
 				}
 			}
 		} else {
 			console.log('Beer not found');
 		}
 	}
-	xhr.send();	
+	xhr.send();
 };
 
